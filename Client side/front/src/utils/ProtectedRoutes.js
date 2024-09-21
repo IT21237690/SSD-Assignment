d778fb752a073b187f10f720befbf0d650df6cab
Bhayga Peramuna <it21241246@my.sliit.lk>
2024-09-22 00:36:49 +0530

    fix: protected route issue

--- Client side/front/src/utils/ProtectedRoutes.js
+++ Client side/front/src/utils/ProtectedRoutes.js
@@ -1,9 +1,13 @@
 import { Outlet, Navigate } from "react-router-dom";
+import { useContext } from "react";
+import { LoginContext } from "../components/ContextProvider/Context"; // adjust the import path if needed
 
 const ProtectedRoutes = () => {
-  const user = null;
-  console.log("user", user);
-  return user ? <Outlet /> : <Navigate to="/mainlogin" />;
+  const { logindata } = useContext(LoginContext); // Fetch the user login data from context
+  const token = localStorage.getItem("usersdatatoken"); // use local storage to check the token
+  console.log("user",logindata )
+  // Check if the user is logged in 
+  return logindata || token ? <Outlet /> : <Navigate to="/mainlogin" />;
 };
 
 export default ProtectedRoutes;
