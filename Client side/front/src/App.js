bd841dcc1501fa537e1730a3f5a5a49a36b282a3
Bhayga Peramuna <it21241246@my.sliit.lk>
2024-09-22 00:16:48 +0530

    feat: added protected routes to client side

--- Client side/front/src/App.js
+++ Client side/front/src/App.js
@@ -1,7 +1,7 @@
 import Header from "./components/Header";
 import Login from "./components/Login";
 import Register from "./components/Register";
-import TeacherRegister from "./components/TeacherReg"
+import TeacherRegister from "./components/TeacherReg";
 import Dashboard from "./components/Dashboard";
 import LoginT from "./components/TeacherLogin";
 import ForgotPassword from "./components/ForgotPasswordteacher";
@@ -14,31 +14,27 @@ import About from "./components/About";
 import NoticeBoard from "./components/notices";
 import Contact from "./components/Contact";
 
-
 import Error from "./components/Error";
 import TDashboard from "./components/teacherdash";
 
-
-import CircularProgress from '@mui/material/CircularProgress';
-import Box from '@mui/material/Box';
-import { Routes, Route, useNavigate } from "react-router-dom"
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import { useEffect, useContext, useState } from "react";
 import { LoginContext } from "./components/ContextProvider/Context";
 
-import WatchCourses from './pages/WatchCourses/WatchCourses';
-import AddPayment from './pages/Payment/AddPayment'
-import EditCourse from './pages/EditCourse/EditCourse';
+import WatchCourses from "./pages/WatchCourses/WatchCourses";
+import AddPayment from "./pages/Payment/AddPayment";
+import EditCourse from "./pages/EditCourse/EditCourse";
 import Course from "./pages/Course/Course";
 
 import Hallallocation from "./components/hallalocation";
-
+import ProtectedRoutes from "./utils/ProtectedRoutes";
 
 function App() {
-
   const [data, setData] = useState(false);
 
   const { logindata, setLoginData } = useContext(LoginContext);
-
 
   const history = useNavigate();
 
@@ -49,8 +45,8 @@ function App() {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": token
-      }
+        Authorization: token,
+      },
     });
 
     const data = await res.json();
@@ -59,10 +55,10 @@ function App() {
       console.log("user not valid");
     } else {
       console.log("user verify");
-      setLoginData(data)
+      setLoginData(data);
       history("/dash");
     }
-  }
+  };
 
   const TDashboardValid = async () => {
     const token = localStorage.getItem("teachersdatatoken");
@@ -91,67 +87,69 @@ function App() {
     }, 2000);
   }, []);
 
-  
   return (
     <>
-      {
-        data ? (
-          <>
-            <Header />
+      {data ? (
+        <>
+          <Header />
 
-            <Routes>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Home />
+                  <About />
+                  <NoticeBoard />
+                  <Contact />
+                </>
+              }
+            />
 
-                            <Route path="/" element={
-                  <>
-                    <Home />
-                    <About />
-                    <NoticeBoard />
-                    <Contact />
-                  </>
-                } />
+            <Route path="/mainlogin" element={<Login />} />
+            <Route path="/Teacher" element={<LoginT />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/Tregister" element={<TeacherRegister />} />
+            <Route path="/dash" element={<Dashboard />} />
+            <Route path="/teacherdash" element={<TDashboard />} />
+            <Route path="/result" element={<ResultPage />} />
 
-              <Route path="/mainlogin" element={<Login />} />
-              <Route path="/Teacher" element={<LoginT />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/Tregister" element={<TeacherRegister />} />
-              <Route path="/dash" element={<Dashboard />} />
-              <Route path="/result" element={<ResultPage />} />
+            <Route path="/fpt" element={<ForgotPassword />} />
+            <Route path="/fps" element={<SutdentForgotPassword />} />
 
-              <Route path='/fpt' element={<ForgotPassword/>}/>
-              <Route path='/fps' element={<SutdentForgotPassword/>}/>
+
+<Route element={<ProtectedRoutes/>}>
+
+
+
+            <Route path="/addresult" element={<UploadForm />} />
+            <Route path="/hall" element={<Hallallocation />} />
+            <Route path="/error" element={<Error />} />
+            {/* <Route path='/payment' element={<AddPayment />} />              */}
+            <Route path="/Student_Courses" element={<WatchCourses />} />
+            <Route path="/Student_Courses/Course/:id" element={<Course />} />
+            <Route
+              path="/Student_Courses/Course/payment/:id"
+              element={<AddPayment />}
+            />
+            <Route path="/allCourse/editCourse" element={<EditCourse />} />
+            </Route>
+          </Routes>
           
-        
-              <Route path="/teacherdash" element={<TDashboard />} />
-              <Route path="/addresult" element={<UploadForm />} />
-
-              <Route path="/hall" element={<Hallallocation />} />
-
-
-
-
-              <Route path="/error" element={<Error />} />
-
-            
-
-              {/* <Route path='/payment' element={<AddPayment />} />              */}
-              <Route path='/Student_Courses' element={<WatchCourses/>}/>
-              <Route path='/Student_Courses/Course/:id' element={<Course  />}/>
-              <Route path='/Student_Courses/Course/payment/:id' element={<AddPayment/>}/>
-              <Route path='/allCourse/editCourse' element={<EditCourse/>}/>
-
-
-
-
-            </Routes>
-          </>
-
-        ) : <Box sx={{ display: 'flex', justifyContent: "center", alignItems: "center", height: "100vh" }}>
+        </>
+      ) : (
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+          }}
+        >
           Loading... &nbsp;
           <CircularProgress />
         </Box>
-      }
-
-
+      )}
     </>
   );
 }
---
+++ Client side/front/src/utils/ProtectedRoutes.js
@@ -0,0 +1,9 @@
+import { Outlet, Navigate } from "react-router-dom";
+
+const ProtectedRoutes = () =>{
+    const user = null
+    console.log("user",user)
+    return user ? <Outlet/> : <Navigate to="/mainlogin"/>
+}
+
+export default ProtectedRoutes;
