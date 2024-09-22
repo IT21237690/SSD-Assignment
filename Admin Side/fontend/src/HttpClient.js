cb9e8d85444249c9be926df30a537cb5f11c8280
Minidu Tissera <IT21237690@my.sliit.lk>
2024-09-23 01:21:10 +0530

    delete

--- Admin Side/fontend/src/HttpClient.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const httpClient = axios.create({
-   
-});
-
-export {httpClient}
--- Admin Side/fontend/src/Keycloak.js
+++
@@ -1,9 +0,0 @@
-// import Keycloak from "keycloak-js";
-
-// const keycloak = new Keycloak({
-//  url: "http://localhost:8080",
-//  realm: "SSD",
-//  clientId: "Web"
-// });
-
-// export default keycloak;
